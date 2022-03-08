3e2c914065b8f292b6152b0ac14b2eb6e355a451
gopimudumal99 <93375587+gopimudumal99@users.noreply.github.com>
2022-03-08 13:44:37 +0530

    sting problem run length encoding

---
+++ alogo/IMP/stringprob.js
@@ -0,0 +1,35 @@
+// RUN LENGTH ENCODING PROBLEN
+// sequence
+let S = "aaabbcccddggee"  // O/P:a3b2c3d2g2e2
+
+// Ap1 key-value pair T.C=O(n) && S.C=O(n)
+function strKeyValu(S) { 
+    let obj = {}
+    let s =""
+    for (var i = 0; i < S.length; i++) { 
+        if (obj[S[i]] == undefined) {
+            obj[S[i]] = 1
+        } else { 
+            obj[S[i]] = obj[S[i]]+1
+        }
+    }
+    for (elem in obj) { 
+        s+=elem+obj[elem]
+    }
+    return s
+}
+console.log(strKeyValu(S)); 
+
+function optiString(S) { 
+    str = ""
+    for (let i = 0; i < S.length; i++) {
+        let c = 1
+        while (i < S.length - 1 && S[i] === S[i + 1]) { 
+            c++
+            i++
+        }
+        str+=S[i]+c
+    }
+    return str
+}
+console.log(optiString(S));
