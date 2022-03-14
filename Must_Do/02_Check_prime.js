0ba502991db285dd6a82f21e4b92868dfca4667b
gopimudumal99 <93375587+gopimudumal99@users.noreply.github.com>
2022-03-14 14:27:35 +0530

    dsa practice

---
+++ Must_Do/02_Check_prime.js
@@ -0,0 +1,22 @@
+// Given an integer N. Check whether N is prime or not.
+
+function isPrime(n) { 
+    let fact = 0
+    for (var i = 1; i <= Math.sqrt(n); i++){
+        // console.log(i)
+        if (n % i == 0) { 
+            if (i * i != n) {
+                fact = fact + 2
+                // console.log(i)
+            } else { 
+                fact = fact + 1
+            }
+        }
+    }
+    if (fact == 2) {
+        console.log("prime")
+    } else { 
+        console.log("no prime")
+    }
+}
+isPrime(4); 
